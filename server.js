6829ec227c97cab2f11bbfa40bbc5cbea41bc62a
Ramish Qamar <95705163+MianRamish@users.noreply.github.com>
2022-07-20 22:20:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "build")));
+  app.use(express.static(path.join(__dirname1, "build/index.html")));
 
   app.get("*", (res) =>
     res.sendFile(path.resolve(__dirname1,  "build", "index.html"))
